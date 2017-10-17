f96a035d110d32e44e00c9360934cebda187eabd
MichaelXu <13240368231@163.com>
2017-10-17 18:00:13 +0800

    投票

---
+++ js/vote.js
@@ -0,0 +1,24 @@
+var topicObj = {
+    init: function() {
+        this.loadMore();
+        this.vote_type_fun();
+    },
+    loadMore: function() {
+        var winH = $(window).height(); //页面可视区域高度 
+        $(window).scroll(function() {
+            var pageH = $(document.body).height();  
+            var scrollT = $(window).scrollTop(); //滚动条top  
+            var aa = (pageH - winH - scrollT) / winH;  
+            if (aa < 0.02) { 
+                $(".loadMore").removeClass("none");
+                //去加载更多
+            }
+         })
+    },
+    vote_type_fun:function(){
+        $("#vote_type_main").on("click","span",function(){
+            $(this).addClass("cur").siblings("span").removeClass("cur");
+        })
+    }
+}
+topicObj.init();
