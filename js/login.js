92a1eaef41695383be8944d4466aeb8e2992922a
MichaelXu <13240368231@163.com>
2017-10-24 17:13:36 +0800

    完整登陆

--- js/login.js
+++ js/login.js
@@ -1,6 +1,37 @@
 var searchObj = {
     init: function() {
-        
+        this.loginFn();
+        this.hidePromptFn();
+    },
+    loginFn:function(){
+        $login_btn=$("#login_btn"),
+        $error_prompt=$("#error_prompt"),
+        $username=$("#username"),
+        $pwd=$("#pwd"),
+        reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+        $login_btn.click(function(){
+            var usernameV=$username.val(),
+                pwdV=$pwd.val();
+            if (!reg.test(usernameV) || usernameV=="") {
+                $error_prompt.html("•邮箱错误！");
+                return false;
+            }else if (pwdV==""){
+                $error_prompt.html("•秘密不能为空！");
+                return false;
+            }else{
+                $error_prompt.html("");
+                common.ajaxFn("ewq","POSt",{},function(){//ajax提交
+
+                });
+            }
+        });
+    },
+    hidePromptFn:function(){
+        var $error_prompt=$("#error_prompt"),
+            ele=$("#username,#pwd");
+            ele.on("focus",function(){
+                $error_prompt.html("");
+            });
     }
     
 }
