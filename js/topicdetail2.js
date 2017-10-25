a5ad91e3e17ad86fda89d6cb6a4eee30f62bd0ac
MichaelXu <13240368231@163.com>
2017-10-25 11:18:50 +0800

    全部专题2

--- js/topicdetail2.js
+++ js/topicdetail2.js
@@ -1,18 +1,6 @@
 var topicObj = {
     init: function() {
-        this.loadMoreFn();
-    },
-    loadMoreFn: function() {
-        var winH = $(window).height(); //页面可视区域高度 
-        $(window).scroll(function() {
-            var pageH = $(document.body).height();  
-            var scrollT = $(window).scrollTop(); //滚动条top  
-            var aa = (pageH - winH - scrollT) / winH;  
-            if (aa < 0.02) { 
-                $(".loadMore").removeClass("none");
-                //去加载更多
-            }
-         })
+       
     }
 }
 topicObj.init();
