bf2b36f13e728f7b3247fb3c21d07d532a4525df
MichaelXu <13240368231@163.com>
2017-10-27 15:35:58 +0800

    加载更多

--- js/topicdetail1.js
+++ js/topicdetail1.js
@@ -2,11 +2,29 @@ var topicObj = {
     init: function() {
         this.loadMoreFn();
         this.TabsChangeFn();
+        this.initAxajFn();
     },
+    isRequest:false,
     TabsChangeFn:function(){
         $("#works_type").on("click","a",function(){
             $(this).addClass("current").siblings("a").removeClass("current")
         })
+    },
+    initAxajFn:function(){
+        common.ajaxFn("../js/test.json","POST","",topicObj.successFn)
+    },
+    successFn:function(res){
+        topicObj.isRequest=true;
+        var $test_more=$("#test_more"),
+            str="";
+        $.each(res.data,function(i,v){
+            str+="<div>姓名："+v.name+"，年龄："+v.age+"</div>"
+        });
+        $test_more.append($(str));
+    },
+    ErrorFn:function(){
+        $(".loadMore").removeClass("none");
+        topicObj.isRequest=false;
     },
     loadMoreFn: function() {
         var winH = $(window).height(); //页面可视区域高度 
@@ -15,8 +33,10 @@ var topicObj = {
             var scrollT = $(window).scrollTop(); //滚动条top  
             var aa = (pageH - winH - scrollT) / winH;  
             if (aa < 0.02) { 
-                $(".loadMore").removeClass("none");
-                //去加载更多
+                if (topicObj.isRequest) {
+                    topicObj.isRequest=false;
+                    common.ajaxFn("../js/test.json","POST","",topicObj.successFn,topicObj.ErrorFn)
+                }
             }
          })
     }
